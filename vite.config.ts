2340fd2cd4e1bcd0e241d9728a93077c7e43a1ad
Ryan Yensch <ry2005@outlook.com>
2026-08-30 02:26:38 +1000

    Added worker config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import preact from '@preact/preset-vite'
 import { defineConfig } from 'vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    cloudflare()
+  ],
 })
